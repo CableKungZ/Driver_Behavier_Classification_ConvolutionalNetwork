10e60ce9d1266236f3106686a09e0e87571cf518
CableKungZ <xdredkillgamertvchannel@gmail.com>
2024-08-27 12:30:31 +0700

    add model

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 import torch
 from PIL import Image
 from prediction import pred_class
@@ -7,15 +7,12 @@ import numpy as np
 # Set title 
 st.title('Driving Behavior Classification')
 
-#Set Header 
+# Set Header 
 st.header('Upload Image')
 
-
-#Load Model 
+# Load Model 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = torch.load('models/EffecienceNet0.pt', map_location=device)
-
-
 
 # Display image & Prediction 
 uploaded_image = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
@@ -24,11 +21,11 @@ if uploaded_image is not None:
     image = Image.open(uploaded_image).convert('RGB')
     st.image(image, caption='Uploaded Image', use_column_width=True)
     
-    class_name = ['other_activities', 'safe_driving','texting_phone', 'talking_phone', 'turning']
+    class_name = ['other_activities', 'safe_driving', 'texting_phone', 'talking_phone', 'turning']
 
     if st.button('Prediction'):
-        #Prediction class
-        probli = pred_class(model,image,class_name)
+        # Prediction class
+        probli = pred_class(model, image, class_name)
         
         st.write("## Prediction Result")
         # Get the index of the maximum value in probli[0]
